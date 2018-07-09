0bf144aba5f6fe0250f04e8594e8829cc250567a
Mike West <mike.wesley.west@gmail.com>
2018-07-08 19:15:46 -0700

    update server-routes

--- routes/server-routes.js
+++ routes/server-routes.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const getArticles = require("../js/articles");
-=======
-const getArticles = require("../articles");
->>>>>>> e628ce4d89b21e7e1c00e6f8f3a69c2ebc592da2
 
 module.exports = function(app) {
     app.get("/api/articles", function(req, res) {
